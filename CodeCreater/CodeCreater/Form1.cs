e65ae3d20419fea76f7e8dd3ef5f5b721786f818
liuzx <liubao-00@163.com>
2013-05-07 22:18:15 +0800

    testCommit
    测试。。。提交。。。

--- CodeCreater/CodeCreater/Form1.cs
+++ CodeCreater/CodeCreater/Form1.cs
@@ -15,5 +15,10 @@ namespace CodeCreater
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("nihao.");
+        }
     }
 }
